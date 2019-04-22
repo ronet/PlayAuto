7d877c6866e1653a7d28300eb26aa0ea86e24d2d
ronet <ronet423@gmail.com>
2019-04-22 14:11:05 +0900

    not promise.all

--- contents.js
+++ contents.js
@@ -4,7 +4,9 @@ const request = require("request"),
 module.exports = async args => {
   console.log("Contents crawling start");
 
-  return Promise.all(args.map(async keyword => {
+  const list = args;
+
+  for (const keyword of list) {
     const url = keyword.url;
     const body = await new Promise((resolve, reject) => {
       request(url, (err, res, body) => {
@@ -14,8 +16,9 @@ module.exports = async args => {
 
     const $ = cheerio.load(body);
 
-    return $("._related_keyword_ul li a").map((i, e) => {
-      return $(e).text();
-    }).get();
-  }));
+    $("._related_keyword_ul li a").map((i, e) => {
+      if (keyword.contents.length < 10) return keyword.contents.push($(e).text());
+    });
+  }
+  return list;
 };
